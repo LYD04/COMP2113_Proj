135d35de50d07e261ec0af1b157cfa5e1c95845c
NeilChung0828 <163509315+NeilChung0828@users.noreply.github.com>
2024-04-29 13:37:32 +0800

    Update cursednum.cpp

--- cursednum.cpp
+++ cursednum.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 //To check if the choice of the player is out of range(lb,ub)
-void validnum(int& s,int a, int b){
-    while(s<a||s>b){
+void validnum(int& s,int a, int b){				//s is the number called by the player or Riddler. a and b ar the lower and upper boundary respectively.
+    while(s<a||s>b){						
         cout<<"*****INVALID INPUT. TRY AGAIN.*****"<<endl;
         cin>>s;
     }
@@ -17,18 +17,18 @@ void validnum(int& s,int a, int b){
 
 
 //To modify the lower and upper boundary according to the player's and the antagonist's choice
-void modifybound(int curnum, int call, int& lb,int& ub){
+void modifybound(int curnum, int call, int& lb,int& ub){	
     if(call>curnum){
-        ub = call-1;
+        ub = call-1;						//E.g. lower boundary (lb) = 20, upper boundary (ub) = 50, cursed number (curnum) = 23. The player called 27. The new boundary is 20 and 26
     }else if(call<curnum){
-        lb = call+1;
+        lb = call+1;						//E.g. lower boundary (lb) = 20, upper boundary (ub) = 50, cursed number (curnum) = 23. The player called 21. The new boundary is 22 and 50
     }
 }
 
 
 //To provide information for the player in the middle of the game and affect the player's decision-making
 void hint(int curnum,int hintnumber, bool real){
-    if(real)
+    if(real)							//The hint could be real or fake, depending on the "real" boolean variable which is randomly determined before the function is called.
     {
         switch(hintnumber)
         {
@@ -147,27 +147,27 @@ bool cursednum() {
     srand(time(NULL));                //Reinitializing
     int curnum = rand()%101;          //Generating the cursed number  
     cout<<"Riddler: ";
-	cout<< "\033[32m";
+	cout<< "\033[32m";	
     cout<<"Let me give you a \"hint\", Batman. BeLIEve it or not! "<<endl;
     cout<< "\033[0m";
-    hint(curnum,rand()%2,rand()%2);
+    hint(curnum,rand()%2,rand()%2);							//rand()%2 generate 0/1 randomly. The first one is to generate the hint number so every time the player might get a different hint. The second one is for the boolean variable that determines if Riddler is telling the truth.
     while(call!=curnum){
         if(turn==-1){
             this_thread::sleep_for(chrono::seconds(1));
             cout<<"*****IT'S YOUR TURN. PLEASE INPUT A NUMBER BETWEEN "<<lb<<" AND "<<ub<<"*****"<<endl;
             cin>>call;
             validnum(call,lb,ub);
-            turn*=-1;
+            turn*=-1;									//Switch to Riddler's turn
         }else if(turn==1){
             this_thread::sleep_for(chrono::seconds(1));
             cout<<"*****IT'S YOUR TURN, Riddler. PLEASE INPUT A NUMBER BETWEEN "<<lb<<" AND "<<ub<<"*****"<<endl;
-            call = lb+rand()%(ub-lb+1); //The antagonist picks a number randomly from the new boundary 
+            call = lb+rand()%(ub-lb+1); 						//The antagonist picks a number randomly from the new boundary 
             this_thread::sleep_for(chrono::seconds(1));
             cout<<"Riddler: ";
 	        cout << "\033[32m";
             cout<<"I picked "<<call<<"."<<endl;
 	        cout << "\033[0m";
-            turn*=-1;
+            turn*=-1;									//Switch to the player's turn
         }
         modifybound(curnum, call, lb, ub);
     }
@@ -175,10 +175,10 @@ bool cursednum() {
     cout << "" << endl;
     cout << "The cursed number was " << curnum << "." << endl;
     //RESULTS
-    if(turn==-1){
+    if(turn==-1){									//Sicne "turn" is multiplied by -1 before exiting the while loop. Now, -1 means it's Riddler's turn. Thus, Riddler lost.
         return true;
     }
-    else if(turn==1){
+    else if(turn==1){									// 1 = The player's turn. Thus, the player lost.
         return false;
     }
    return false;
